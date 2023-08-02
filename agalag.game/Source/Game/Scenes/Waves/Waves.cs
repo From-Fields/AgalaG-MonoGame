edd42cc2d6315906d3a9d41b6e38423da51104e8
Edglatus <tabrinath@gmail.com>
2023-08-02 20:08:25 -0300

    feat: new wave: Fly-By Night

--- agalag.game/Source/Game/Scenes/Waves/Waves.cs
+++ agalag.game/Source/Game/Scenes/Waves/Waves.cs
@@ -119,14 +119,76 @@ namespace agalag.game.scenes
                 }
             );
 
+            Wave asteroidClock = new Wave
+            (
+                5,
+                new List<iWaveUnit>()
+                {
+                    new WaveHazard(new Vector2(width * 1.1f, height * 0.9f), new Vector2(-0.75f, -0.75f), maxBounces: 9),
+                    new WaveHazard(new Vector2(width * 0.7f, -height * 0.6f), new Vector2(-0.25f, 1.25f), maxBounces: 7),
+                    new WaveHazard(new Vector2(-width * 0.4f, height * 0.1f), new Vector2(1f, 0.25f), maxBounces: 5),
+                    new WaveHazard(new Vector2(-width * 0.2f, height * 1.9f), new Vector2(0.75f, -1.5f), maxBounces: 10),
+                }
+            );
+
+            Wave flyByNight = new Wave
+            (
+                15, 
+                new List<iWaveUnit>() 
+                {
+                    new WaveUnit<EnemyGemini>
+                    (
+                        new Vector2(width * 0.2f, -height * 0.2f),
+                        new WaitSeconds(1),
+                        new MoveAndShoot(new Vector2(-width * 0.2f, height * 0.8f)),
+                        new Queue<iEnemyAction>(new iEnemyAction[] { 
+                            new MoveTowards(new Vector2(width * 0.8f, height * 0.3f)),
+                            new Shoot(4),
+                        }),
+                        onDeath
+                    ),
+                    new WaveUnit<EnemyGemini>
+                    (
+                        new Vector2(width * 1.2f, -height * 0.2f),
+                        new WaitSeconds(2),
+                        new MoveAndShoot(new Vector2(width * 1.2f, height * 0.8f)),
+                        new Queue<iEnemyAction>(new iEnemyAction[] { 
+                            new MoveTowards(new Vector2(width * 0.2f, height * 0.3f)),
+                            new Shoot(3),
+                        }),
+                        onDeath
+                    ),
+                    new WaveUnit<EnemyBumblebee>
+                    (
+                        new Vector2(-width * 0.1f, height * 0.25f),
+                        new MoveTowards(new Vector2(width * 0.15f, height * 0.25f)), 
+                        new MoveAndShoot(new Vector2(width * 1.2f, height * 0.25f)),
+                        new Queue<iEnemyAction>(new iEnemyAction[] {
+                            new Shoot(1)
+                        }),
+                        onDeath
+                    ),
+                    new WaveUnit<EnemyBumblebee>
+                    (
+                        new Vector2(width * 1.1f, height * 0.4f),
+                        new MoveTowards(new Vector2(width * 0.85f, height * 0.4f)), 
+                        new MoveAndShoot(new Vector2(-width * 0.2f, height * 0.4f)),
+                        new Queue<iEnemyAction>(new iEnemyAction[] {
+                            new Shoot(1)
+                        }),
+                        onDeath
+                    )
+                }
+            );
+
             waves.AddRange(
                 new List<WaveController>()
                     {
-                        new WaveController(doubleKami.timeout, levelBounds, doubleKami.units),
-                        new WaveController(bumbleTrouble.timeout, levelBounds, bumbleTrouble.units),
-                        new WaveController(geminiSentry.timeout, levelBounds, geminiSentry.units),
+                        // new WaveController(doubleKami.timeout, levelBounds, doubleKami.units),
+                        // new WaveController(bumbleTrouble.timeout, levelBounds, bumbleTrouble.units),
+                        // new WaveController(geminiSentry.timeout, levelBounds, geminiSentry.units),
                         // new WaveController(asteroidClock.timeout, levelBounds, asteroidClock.units),
-                        // new WaveController(flyByNight.timeout, levelBounds, flyByNight.units),
+                        new WaveController(flyByNight.timeout, levelBounds, flyByNight.units),
                         // new WaveController(simmetry.timeout, levelBounds, simmetry.units),
                         // new WaveController(tsuKami.timeout, levelBounds, tsuKami.units),
                         // new WaveController(pincerBlow.timeout, levelBounds, pincerBlow.units),
